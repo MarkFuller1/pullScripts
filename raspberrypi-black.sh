b8a68d41b80e5c7cf604251ddec6972be7a27131
Mark Fuller <38447333+MarkFuller1@users.noreply.github.com>
2022-05-17 19:08:18 -0700

    Update raspberrypi-black.sh

--- raspberrypi-black.sh
+++ raspberrypi-black.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
+if [[ -d "/opt/prod/raspi/Personal-Website-PostService" ]]; then
+    cd Personal-Website-PostService
+    git pull origin master
+    else
+    git clone https://github.com/MarkFuller1/Personal-Website-PostService.git
+    cd Personal-Website-PostService
+fi
 
-y | rm PersonalWebsite -rf
-git clone https://github.com/MarkFuller1/Personal-Website-PostService.git
-
-cd Personal-Website-PostService
 sudo /usr/bin/mvn clean compile package 
-echo "done compile package"
-echo "start server"
-sudo /usr/bin/java -jar -Dspring.profiles.active=prod target/node-0.0.1-SNAPSHOT.jar
+
+sudo /usr/bin/java -jar -Dspring.profiles.active=prod target/IThoughtILearned-0.0.1-SNAPSHOT.jar
