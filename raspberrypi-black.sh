b0d35be31c2b571c98410d916c98c99416677cd5
Mark Fuller <38447333+MarkFuller1@users.noreply.github.com>
2022-05-17 19:32:13 -0700

    Update raspberrypi-black.sh

--- raspberrypi-black.sh
+++ raspberrypi-black.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
+pwd
 if [[ -d "/opt/prod/raspi/Personal-Website-PostService" ]]; then
     cd Personal-Website-PostService
+    pwd
     git pull origin master
     else
     git clone https://github.com/MarkFuller1/Personal-Website-PostService.git
     cd Personal-Website-PostService
+    pwd
 fi
 
 sudo /usr/bin/mvn clean compile package 
