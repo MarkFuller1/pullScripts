4a398c3ca9d5c04feac1d0fba2cb162b9b027d4b
Mark Fuller <38447333+MarkFuller1@users.noreply.github.com>
2022-05-17 19:03:21 -0700

    Update raspberrypi-clear.sh

--- raspberrypi-clear.sh
+++ raspberrypi-clear.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
+if [[ -f "/opt/prod/raspi/Personal-Website" ]]; then
+    cd Personal-Website
+    git pull origin master
+    else
+    git clone https://github.com/MarkFuller1/Personal-Website.git
+    cd Personal-Website
+fi
 
-y | rm Personal-Website -rf
-git clone https://github.com/MarkFuller1/Personal-Website.git
-
-cd Personal-Website
-npm i
+npm i --legacy-peer-deps
 npm start &
