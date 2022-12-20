eb7840f4c75146efb0b60e843ca0f338f12ef4bd
Mark Fuller <Mark_Fuller1@baylor.edu>
2022-12-20 11:10:14 -0800

    update clear pullscript

--- raspberrypi-clear.sh
+++ raspberrypi-clear.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-if [[ -d "/opt/prod/raspi/Personal-Website" ]]; then
-    cd Personal-Website
+if [[ -d "/opt/prod/raspi/KittyProcessor" ]]; then
+    cd KittyProcessor
     git pull origin master
     else
-    git clone https://github.com/MarkFuller1/Personal-Website.git
-    cd Personal-Website
+    git clone https://github.com/MarkFuller1/KittyProcessor.git
+    cd KittyProcessor
 fi
 
 npm i --legacy-peer-deps
