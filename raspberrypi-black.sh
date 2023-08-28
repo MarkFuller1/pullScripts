556d059c0936abf334299dd3b329c135d5b90709
Mark Fuller <38447333+MarkFuller1@users.noreply.github.com>
2023-08-28 12:39:26 -0700

    Update raspberrypi-black.sh

--- raspberrypi-black.sh
+++ raspberrypi-black.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 pwd
-if [[ -d "/opt/prod/raspi/KittyKam" ]]; then
-    cd KittyKam
+if [[ -d "/opt/prod/raspi/HomeInfrastructure" ]]; then
+    cd HomeInfrastructure
     pwd
     git pull origin master
     else
-    git clone https://github.com/MarkFuller1/KittyKam.git
-    cd KittyKam
+    git clone https://github.com/MarkFuller1/HomeInfrastructure.git
+    cd HomeInfrastructure
     pwd
 fi
 
 sudo /usr/bin/mvn clean compile package 
 
-sudo /usr/bin/java -jar -Dspring.profiles.active=prod target/IThoughtILearned-0.0.1-SNAPSHOT.jar
+sudo /usr/bin/java -jar -Dspring.profiles.active=prod target/HomeInfrastructure-1.0-SNAPSHOT.jar
